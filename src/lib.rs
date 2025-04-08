084cf59f98ace6bf2289e266a6c68ca22ab59fc4
cleveng <cleveng@gmail.com>
2025-04-08 18:47:30 +0800

    perf: account token

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,35 @@ pub mod constants;
 pub mod official_account;
 
 use deadpool_redis::{Config, Pool, Runtime};
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
 use std::sync::Arc;
 
 pub struct OfficialAccount {
     appid: String,
     app_secret: String,
     rdb_pool: Arc<Pool>,
+    client: Client,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct TokenResponse {
+    pub access_token: String,
+    expires_in: u64,
 }
 
 impl OfficialAccount {
+    /// Creates a new instance of the OfficialAccount struct.
+    ///
+    /// # Arguments
+    ///
+    /// * `appid` - The appid of the WeChat application.
+    /// * `app_secret` - The app secret of the WeChat application.
+    /// * `cfg` - The URL of the Redis database connection string.
+    ///
+    /// # Returns
+    ///
+    /// A new instance of the OfficialAccount struct.
     pub fn new(appid: String, app_secret: String, cfg: String) -> Self {
         let pool_config = Config::from_url(cfg);
 
@@ -25,6 +45,7 @@ impl OfficialAccount {
             appid,
             app_secret,
             rdb_pool,
+            client: Client::new(),
         }
     }
 }
--- src/official_account/mod.rs
+++ src/official_account/mod.rs
@@ -1,2 +1,3 @@
 pub mod core;
 pub mod qrcode;
+pub mod token;
