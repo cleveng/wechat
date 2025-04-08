97d6598d1d7b569bb96a8e4edf92c2385b4f9bfa
cleveng <cleveng@gmail.com>
2025-04-08 18:49:25 +0800

    perf: update qrcode

--- src/official_account/qrcode.rs
+++ src/official_account/qrcode.rs
@@ -1,98 +1,110 @@
-use std::collections::HashMap;
+use crate::OfficialAccount;
 
-use crate::{OfficialAccount, constants::keys};
-
-use deadpool_redis::redis::cmd;
-
-use reqwest::Client;
 use serde::{Deserialize, Serialize};
-use url::{Url, form_urlencoded};
+use urlencoding::encode;
+
+const QR_CREATE_URL: &str = "https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token=";
+const QR_IMG_URL: &str = "https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=";
 
 #[cfg(test)]
 mod tests {
-    use crate::OfficialAccount;
+    use uuid::Uuid;
+
+    use crate::{
+        OfficialAccount,
+        official_account::qrcode::{self, TicketRequest},
+    };
     use std::env;
 
     #[tokio::test]
-    async fn get_access_token() {
+    async fn get_qr_ticket() {
         dotenv::dotenv().ok();
 
-        let appid = env::var("APPID").expect("APPID not set");
-        let app_secret = env::var("APP_SECRET").expect("APP_SECRET not set");
-        let cfg = env::var("REDIS_URL").expect("APP_CFG not set");
+        let appid = env::var("APPID").expect("appid not set");
+        let app_secret = env::var("APP_SECRET").expect("app secret not set");
+        let cfg = env::var("REDIS_URL").expect("redis url not set");
 
         let account = OfficialAccount::new(appid, app_secret, cfg);
-        let at = account.get_qr_ticket("123".to_string()).await;
 
-        println!("get_access_token: {:#?}", at.unwrap());
+        let key = format!("login:{}", Uuid::new_v4().to_string());
+
+        let scene = qrcode::TicketScene {
+            scene_id: None,
+            scene_str: Some(key.clone()),
+        };
+        let params = TicketRequest {
+            expire_seconds: 200000,
+            action_name: qrcode::TicketActionName::QR_STR_SCENE,
+            action_info: qrcode::TicketActionInfo { scene },
+        };
+
+        let at = account.get_qr_ticket(params).await;
+        println!("get_qr_ticket: {:#?}", at.unwrap().show_qrcode());
     }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct TokenResponse {
-    access_token: String,
-    expires_in: u64,
+pub enum TicketActionName {
+    QR_SCENE,
+    QR_STR_SCENE,
+    QR_LIMIT_SCENE,
+    QR_LIMIT_STR_SCENE,
+}
+
+impl TicketActionName {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            TicketActionName::QR_SCENE => "QR_SCENE",
+            TicketActionName::QR_STR_SCENE => "QR_STR_SCENE",
+            TicketActionName::QR_LIMIT_SCENE => "QR_LIMIT_SCENE",
+            TicketActionName::QR_LIMIT_STR_SCENE => "QR_LIMIT_STR_SCENE",
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct TicketActionInfo {
+    pub scene: TicketScene,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct TicketScene {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub scene_id: Option<u32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub scene_str: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct TicketRequest {
+    expire_seconds: u64,
+    action_name: TicketActionName,
+    action_info: TicketActionInfo,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct TicketResponse {
     pub ticket: String,
-    pub expire_seconds: i64,
+    pub expire_seconds: u64,
     pub url: String,
 }
 
-impl OfficialAccount {
-    /// [Retrieves the access token for the official account](https://developers.weixin.qq.com/doc/offiaccount/Basic_Information/Get_access_token.html)
+impl TicketResponse {
+    /// Generates a URL to display the QR code using the ticket.
     ///
-    /// This function attempts to fetch the access token from the Redis database.
-    /// If not found, it requests a new token from the WeChat API using the
-    /// application ID and secret, then stores the token in Redis for future use.
+    /// This function encodes the ticket and constructs a URL that can be used to
+    /// view the QR code associated with the provided ticket.
     ///
     /// # Returns
     ///
-    /// * A `Result` containing the access token as a `String` on success, or a boxed
-    ///   error on failure.
-    ///
-    /// # Errors
-    ///
-    /// * Returns an error if the Redis operation fails, if the HTTP request fails,
-    ///   or if the response cannot be deserialized into a `TokenResponse`.
-    async fn token(&self) -> Result<String, Box<dyn std::error::Error>> {
-        let mut rdb = self.rdb_pool.get().await?;
-
-        if let Ok(token) = cmd("GET")
-            .arg(keys::GLOBAL_TOKEN)
-            .query_async::<String>(&mut rdb)
-            .await
-        {
-            return Ok(token);
-        }
-
-        let mut url = Url::parse("https://api.weixin.qq.com/cgi-bin/token").unwrap();
-        let query = form_urlencoded::Serializer::new(String::new())
-            .append_pair("appid", &self.appid)
-            .append_pair("secret", &self.app_secret)
-            .append_pair("grant_type", "client_credential")
-            .finish();
-
-        url.set_query(Some(&query));
-        let url = url.to_string();
-
-        let client = Client::new();
-        let response = client.get(&url).send().await?;
-
-        let at: TokenResponse = response.json::<TokenResponse>().await?;
-        cmd("SETEX")
-            .arg(keys::GLOBAL_TOKEN)
-            .arg(60 * 50 * 2)
-            .arg(serde_json::to_string(&at).unwrap())
-            .query_async::<()>(&mut rdb)
-            .await
-            .unwrap();
-
-        Ok(at.access_token)
+    /// * A `String` representing the URL to display the QR code.
+    pub fn show_qrcode(&self) -> String {
+        let ticket = encode(&self.ticket);
+        format!("{}{}", QR_IMG_URL, ticket)
     }
+}
 
+impl OfficialAccount {
     /// [Create a QR code ticket for the official account](https://developers.weixin.qq.com/doc/offiaccount/Account_Management/Generating_a_Parametric_QR_Code.html)
     ///
     /// This function creates a QR code ticket for the official account and returns
@@ -104,28 +116,26 @@ impl OfficialAccount {
     ///   or if the response cannot be deserialized into a `String`.
     pub async fn get_qr_ticket(
         &self,
-        scene: String,
+        req: TicketRequest,
     ) -> Result<TicketResponse, Box<dyn std::error::Error>> {
         let token = self.token().await?;
 
-        println!("token: {}", token);
+        let url = format!("{}{}", QR_CREATE_URL, token);
+        let response = match self.client.post(url).json(&req).send().await {
+            Ok(r) => r,
+            Err(e) => {
+                println!("json err: {:#?}", e);
+                return Err(e.into());
+            }
+        };
 
-        let mut params = HashMap::new();
-        params.insert("expire_seconds".to_string(), "604800".to_string());
-        params.insert("action_name".to_string(), "QR_STR_SCENE".to_string());
-        params.insert(
-            "action_info".to_string(),
-            serde_json::json!({ "scene": { "scene_str": scene } }).to_string(),
-        );
-
-        let url = format!(
-            "https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token={}",
-            token
-        );
-
-        let client = Client::new();
-        let response = client.post(url).json(&params).send().await?;
-        let result = response.json::<TicketResponse>().await?;
+        let result = match response.json::<TicketResponse>().await {
+            Ok(r) => r,
+            Err(e) => {
+                println!("json err: {:#?}", e);
+                return Err(e.into());
+            }
+        };
 
         Ok(result)
     }
